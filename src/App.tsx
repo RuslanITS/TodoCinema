19b9965d56eff986dba1b7987f17263c01e4cc59
Ruslan <rcernov442@gmail.com>
2026-05-22 00:30:35 +0600

    Task-1: add todo form and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,87 @@
-import './App.css'
+import "./App.css";
+import { type ChangeEvent, type SubmitEventHandler, useState } from "react";
+
+type Task = {
+  id: string;
+  text: string;
+};
 
 const App = () => {
+  const [renderList, setRenderList] = useState<Task[]>([
+    { id: '123', text: "movie cinema one" },
+    { id: '124', text: "movie cinema two" },
+    { id: '125', text: "movie cinema three" },
+  ]);
+  const [task, setTask] = useState<string>("");
 
-  const allTasks = [
-    { id: 123, text: 'move cinema one' },
-    { id: 124, text: 'move cinema two' },
-    { id: 125, text: 'move cinema tre' }
-  ]
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = (
+    event
+  ) => {
+    event.preventDefault();
+
+    if (!task.trim()) return;
+
+    const newTask: Task = {
+      id: crypto.randomUUID(),
+      text: task,
+    };
+
+    setRenderList([...renderList, newTask]);
+
+    setTask("");
+  };
+
+  const handleDelete = (id: string) => {
+    setRenderList(
+      renderList.filter((item) => item.id !== id)
+    );
+  };
 
   return (
     <div className="container py-5">
-
       <div className="card shadow p-4">
+        <h1 className="mb-4 text-center">Todo Cinema App</h1>
 
-        <h1 className="mb-4 text-center">Todo App</h1>
-
-        <form className="d-flex gap-2 mb-4">
-
+        <form
+          onSubmit={handleSubmit}
+          className="d-flex gap-2 mb-4"
+        >
           <input
-            id="task"
-            name="task"
             type="text"
             className="form-control"
             placeholder="Add new task..."
+            value={task}
+            onChange={(
+              event: ChangeEvent<HTMLInputElement>
+            ) => setTask(event.target.value)}
           />
 
-          <button className="btn btn-primary">Add</button>
+          <button className="btn btn-primary">
+            Add
+          </button>
         </form>
 
         <ul className="list-group">
-
-          {allTasks.map((task, index) => (
+          {renderList.map((item, index) => (
             <li
-              key={task.id}
+              key={item.id}
               className="list-group-item d-flex justify-content-between align-items-center"
             >
-              <span>{index + 1}. {task.text}</span>
-
-              <button className="btn btn-danger btn-sm">Delete</button>
+              <span>
+                {index + 1}. {item.text}
+              </span>
+              <button
+                onClick={() => handleDelete(item.id)}
+                className="btn btn-danger btn-sm"
+              >
+                Delete
+              </button>
             </li>
           ))}
-
         </ul>
-
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
